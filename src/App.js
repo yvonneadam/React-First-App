a1955be950030ec9519040c5db4af9bcc0a943b3
Yvonne Adam <yvonne_adam@t-online.de>
2022-06-22 11:28:25 +0200

    first init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const HelloMuc = () => {
+  return <p>Hello Muc-3</p>;
+};
+
+const HelloHeader = () => {
+  return <hi>HI, ich bin ein nicer Header.</hi>;
+};
+
+const HelloHeader2 = () => {
+  return <h2>Und dann kommt das Logo</h2>;
+};
+
+const HelloFooter = () => {
+  return (
+    <footer className="app__footer">
+      <nav>
+        <ul>
+          <li>Person1</li>
+          <li>Person2</li>
+          <li>Person3</li>
+        </ul>
+      </nav>
+    </footer>
+  );
+};
 
 function App() {
+  // comment here hinschreiben //
   return (
     <div className="App">
+      {/* comment here schreiben */}
       <header className="App-header">
+        <HelloHeader />
+        <HelloHeader2 />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <HelloMuc />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +46,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <HelloFooter />
     </div>
   );
 }
